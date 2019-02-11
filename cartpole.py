abf9372987d678eaab2c77bba58b104f471eacbc
atsuhiro-narita <alayashiki@gmail.com>
2019-02-12 00:13:43 +0900

    --wip--

--- cartpole.py
+++ cartpole.py
@@ -5,11 +5,13 @@ from dataclasses import dataclass
 from typing import List
 import numpy as np
 
+
 # https://qiita.com/animegazer/items/4158462e5a3efaba9d7b
 
 @dataclass
 class ProductionPlan:
     sozai: float
+
 
 @dataclass
 class State:
@@ -29,7 +31,6 @@ class ProductionScheduling:
         self.state = State(sozai=0, buhin=0, seihin=0)
         pass
 
-
     def reset(self):
         pass
 
@@ -37,9 +38,8 @@ class ProductionScheduling:
         pass
 
     def step(self, action: ProductionPlan):
-        #(observation, reward, done, info)
+        # (observation, reward, done, info)
         pass
-
 
 
 class TDGradientAgent:
@@ -51,7 +51,8 @@ def do_nothing(env):
     env.reset()
     for _ in range(1000):
         env.render()
-        env.step(env.action_space.sample()) # take a random action
+        env.step(env.action_space.sample())  # take a random action
+
 
 def random_action(env):
     for i_episode in range(20):
@@ -62,11 +63,73 @@ def random_action(env):
             action = env.action_space.sample()
             observation, reward, done, info = env.step(action)
             if done:
-                print("Episode finished after {} timesteps".format(t+1))
+                print("Episode finished after {} timesteps".format(t + 1))
                 break
 
+@dataclass
+class DiscreteCartPole(gym.core.Env):
+    underlying: Env
 
-if __name__=="__main__":
+    def __init__(self, env: gym.core.Env):
+        self.underlying = env
+
+    def action_space(self):
+        return self.underlying.action_space
+
+    def observation_space(self):
+        return self.underlying.observation_space
+
+    def metadata(self):
+        return self.underlying.metadata
+
+    def reward_range(self):
+        return self.underlying.reward_range
+
+    def step(self, action):
+        observation, reward, done, info = self.underlying.step(action)
+        self._discretize(observation), reward, done, info
+
+
+    def _discretize(self, observation):
+        return observation
+
+
+
+class OnPolicyQAgent:
+    import numpy as np
+    from gym.core import Env
+
+    env: Env
+
+    # TODO: actionを選ぶ部分を分離
+    # TODO: alpha決定する部分を分離
+
+    q_table: np.ndarray
+    maxT: int = 100
+    trial: int = 1000
+
+    def __init__(self):
+
+        self.q_table: np.ndarray = np.zeros(())
+
+        pass
+
+    def train(self):
+        for _ in range(self.trial):
+            for t in range(self.maxT):
+                env.render()
+                print(observation)
+                action = env.action_space.sample()
+                observation, reward, done, info = env.step(action)
+                if done:
+                    print("Episode finished after {} timesteps".format(t + 1))
+                    break
+
+        pass
+
+
+if __name__ == "__main__":
     env = gym.make('CartPole-v0')
-    #do_nothing(env)
-    random_action(env)
+    print(type(env))
+    # do_nothing(env)
+    # random_action(env)
